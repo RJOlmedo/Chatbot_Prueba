fe5d8ca62a450804cca9924f14517c054742d3ba
Ricky Olmedo <rjolmedoc@gmail.com>
2024-10-09 11:21:32 -0600

    Change Order of Messages

--- src/templates/registerFlow.ts
+++ src/templates/registerFlow.ts
@@ -17,6 +17,63 @@ const registerFlow = addKeyword(EVENTS.ACTION)
         await ctxFn.flowDynamic("Perfecto " + ctx.body + " 🎉");
         await ctxFn.state.update({ "name": ctx.body });
     })
+    .addAnswer('¿Que Rango de edad tienes?', { capture: false },
+        async (ctx, { provider }) => {
+              const list = {
+                "header": {
+                  "type": "text",
+                  "text": "Lista de Rangos"
+                },
+                "body": {
+                  "text": "Presiona abajo para ver las opciones"
+                },
+                "footer": {
+                  "text": ""
+                },
+                "action": {
+                  "button": "Opciones",
+                  "sections": [
+                    {
+                      "title": "Rango de edades: ",
+                      "rows": [
+                        {
+                          "id": "<LIST_SECTION_1_ROW_1_ID>",
+                          "title": "18-25 años",
+                          "description": ""
+                        },
+                        {
+                          "id": "<LIST_SECTION_1_ROW_2_ID>",
+                          "title": "26-35 años",
+                          "description": ""
+                        },
+                        {
+                            "id": "<LIST_SECTION_1_ROW_3_ID>",
+                            "title": "36-45 años",
+                            "description": ""
+                          },
+                          {
+                            "id": "<LIST_SECTION_1_ROW_4_ID>",
+                            "title": "46-55 años",
+                            "description": ""
+                          },
+                          {
+                            "id": "<LIST_SECTION_1_ROW_5_ID>",
+                            "title": "56-65 años",
+                            "description": ""
+                          },
+                          {
+                            "id": "<LIST_SECTION_1_ROW_6_ID>",
+                            "title": "65 años o más",
+                            "description": ""
+                          }
+                      ]
+                    },
+                  ]
+                }
+              };
+              await provider.sendList(ctx.from, list);
+            })    
+
     .addAnswer('Ahora, ¿cuál es tu mail?', { capture: true }, 
     async (ctx, ctxFn) => {
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -26,63 +83,8 @@ const registerFlow = addKeyword(EVENTS.ACTION)
         const state = ctxFn.state.getMyState();
         await sheetsService.createUser(ctx.from, state.name, ctx.body);
         await ctxFn.flowDynamic("Excelente! Tus datos ya fueron cargados, ya podes comenzar a utilizar el servicio. 🚀");
-    })
+    });
 
-    .addAnswer('¿Que Rango de edad tienes?', { capture: false },
-    async (ctx, { provider }) => {
-          const list = {
-            "header": {
-              "type": "text",
-              "text": "Lista de Rangos"
-            },
-            "body": {
-              "text": "Presiona abajo para ver las opciones"
-            },
-            "footer": {
-              "text": ""
-            },
-            "action": {
-              "button": "Opciones",
-              "sections": [
-                {
-                  "title": "Rango de edades: ",
-                  "rows": [
-                    {
-                      "id": "<LIST_SECTION_1_ROW_1_ID>",
-                      "title": "18-25 años",
-                      "description": ""
-                    },
-                    {
-                      "id": "<LIST_SECTION_1_ROW_2_ID>",
-                      "title": "26-35 años",
-                      "description": ""
-                    },
-                    {
-                        "id": "<LIST_SECTION_1_ROW_3_ID>",
-                        "title": "36-45 años",
-                        "description": ""
-                      },
-                      {
-                        "id": "<LIST_SECTION_1_ROW_4_ID>",
-                        "title": "46-55 años",
-                        "description": ""
-                      },
-                      {
-                        "id": "<LIST_SECTION_1_ROW_5_ID>",
-                        "title": "56-65 años",
-                        "description": ""
-                      },
-                      {
-                        "id": "<LIST_SECTION_1_ROW_6_ID>",
-                        "title": "65 años o más",
-                        "description": ""
-                      }
-                  ]
-                },
-              ]
-            }
-          };
-          await provider.sendList(ctx.from, list);
-        });      
+    
 
 export { registerFlow };
